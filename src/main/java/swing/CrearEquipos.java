b1206e20c6865838fa9f7f5010e2a6cfbfd7945f
JorgeIs-01 <72407493+JorgeIs-01@users.noreply.github.com>
2021-03-07 20:36:11 +0100

    repetir nombre solucionado

--- src/main/java/swing/CrearEquipos.java
+++ src/main/java/swing/CrearEquipos.java
@@ -18,7 +18,7 @@ import javax.swing.JPanel;
 import javax.swing.JScrollBar;
 import javax.swing.JTextField;
 import java.awt.Color;
-import java.awt.Font; 
+import java.awt.Font;
 import javax.swing.JOptionPane;
 import javax.swing.text.AttributeSet;
 import javax.swing.text.BadLocationException;
@@ -35,53 +35,46 @@ public class CrearEquipos extends javax.swing.JFrame {
      */
     int contJugadores = -1;
     private ControlPartida control;
-    private String[] tipos = new String[]{"Elige planeta","Normal","Sejuani","Gigante","Rojo","Azul","Verde","Vampiro","Zombie"};
-  
+    private String[] tipos = new String[]{"Elige planeta", "Normal", "Sejuani", "Gigante", "Rojo", "Azul", "Verde", "Vampiro", "Zombie"};
+
     public CrearEquipos() {
         initComponents();
         jPanel1.setOpaque(true);
-       JLabel myLabel = new JLabel();
+        JLabel myLabel = new JLabel();
 
-
-        
     }
 
     CrearEquipos(ControlPartida aThis) {
-        
-        
-       initComponents();
-       
+
+        initComponents();
+
         jPanel1.setOpaque(false);
         this.setResizable(false);
-        this.setBounds(450,200,1027,550);
-       
-        
-        
-       control = aThis;
+        this.setBounds(450, 200, 1027, 550);
+
+        control = aThis;
     }
-    
-    private void addEquipo(int a){
-      
-        
-        
+
+    private void addEquipo(int a) {
+
         for (int i = 0; i < a; i++) {
             contJugadores++;
             JLabel jLabel = new javax.swing.JLabel();
             JPanel jPanel = new javax.swing.JPanel();
-             
+
             jPanel.setOpaque(false);
-            
+
             final JLabel jLabelimg = new javax.swing.JLabel();
             JTextField jTextField = new JTextField();
             JComboBox<String> jComboBox = new JComboBox<String>();
             jPanel.setMinimumSize(new java.awt.Dimension(133, 20));
-            
+
             Font fuente = new Font("Calibri", 3, 19);
             jLabel.setFont(fuente);
-            jLabel.setText("Planeta "+(contJugadores+1)+":");
+            jLabel.setText("Planeta " + (contJugadores + 1) + ":");
             jPanel.add(jLabel);
             jLabelimg.setIcon(new javax.swing.ImageIcon("src\\main\\java\\img\\QuestionMarkSquare64.png")); // NOI18N
-            
+
             jTextField.setColumns(25);
             jTextField.setDocument(new JTextFieldLimit(10));
             jPanel.add(jTextField);
@@ -89,40 +82,34 @@ public class CrearEquipos extends javax.swing.JFrame {
             jComboBox.setModel(new javax.swing.DefaultComboBoxModel<>(tipos));
             jPanel.add(jComboBox);
             jPanel.add(jLabelimg);
-          
+
             jPanel1.add(jPanel);
-            
-            
-            
-            
+
             jComboBox.addActionListener((arg0) -> {
 
                 if ("Sejuani".equals(jComboBox.getSelectedItem())) {
                     jLabelimg.setIcon(new javax.swing.ImageIcon("src\\main\\java\\img\\SejuaniSquare64.png")); // NOI18N
-                }else if ("Normal".equals(jComboBox.getSelectedItem())) {
+                } else if ("Normal".equals(jComboBox.getSelectedItem())) {
                     jLabelimg.setIcon(new javax.swing.ImageIcon("src\\main\\java\\img\\MissFortuneSquare64.png")); // NOI18N
-                }else if ("Elije planeta".equals(jComboBox.getSelectedItem())) {
+                } else if ("Elije planeta".equals(jComboBox.getSelectedItem())) {
                     jLabelimg.setIcon(new javax.swing.ImageIcon("src\\main\\java\\img\\QuestionMarkSquare64.png")); // NOI18N
-                }else if ("Gigante".equals(jComboBox.getSelectedItem())) {
+                } else if ("Gigante".equals(jComboBox.getSelectedItem())) {
                     jLabelimg.setIcon(new javax.swing.ImageIcon("src\\main\\java\\img\\GragasSquare64.png")); // NOI18N
-                }else if ("Azul".equals(jComboBox.getSelectedItem())) {
+                } else if ("Azul".equals(jComboBox.getSelectedItem())) {
                     jLabelimg.setIcon(new javax.swing.ImageIcon("src\\main\\java\\img\\FizzSquare64.png")); // NOI18N
-                }else if ("Rojo".equals(jComboBox.getSelectedItem())) {
+                } else if ("Rojo".equals(jComboBox.getSelectedItem())) {
                     jLabelimg.setIcon(new javax.swing.ImageIcon("src\\main\\java\\img\\BrandSquare64.png")); // NOI18N
-                }else if ("Verde".equals(jComboBox.getSelectedItem())) {
+                } else if ("Verde".equals(jComboBox.getSelectedItem())) {
                     jLabelimg.setIcon(new javax.swing.ImageIcon("src\\main\\java\\img\\IvernSquare64.png")); // NOI18N
-                }else if ("Vampiro".equals(jComboBox.getSelectedItem())) {
+                } else if ("Vampiro".equals(jComboBox.getSelectedItem())) {
                     jLabelimg.setIcon(new javax.swing.ImageIcon("src\\main\\java\\img\\VladimirSquare.png")); // NOI18N
-                }else if ("Zombie".equals(jComboBox.getSelectedItem())) {
+                } else if ("Zombie".equals(jComboBox.getSelectedItem())) {
                     jLabelimg.setIcon(new javax.swing.ImageIcon("src\\main\\java\\img\\AmumuSquare.png")); // NOI18N
                 }
-          
-            
-            
-            }              
-);
-            
-            
+
+            }
+            );
+
         }
     }
 
@@ -178,118 +165,79 @@ public class CrearEquipos extends javax.swing.JFrame {
 
     private void btn_addActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_addActionPerformed
         // TODO add your handling code here:
-       if (contJugadores<5){
-           addEquipo(1);
-        pack(); 
-           
-       }
-        
-       
-       
+        if (contJugadores < 5) {
+            addEquipo(1);
+            pack();
+
+        }
+
+
     }//GEN-LAST:event_btn_addActionPerformed
 
     private void btn_createActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_createActionPerformed
 
-        List<Planeta> planetas  = new ArrayList<>();
+        List<Planeta> planetas = new ArrayList<>();
         List Nplanetas = new ArrayList();
-        List Nplanetas2 = new ArrayList();
         // TODO add your handling code here:
-        
-        
 
         for (int i = 0; i < jPanel1.getComponents().length; i++) {
             String NombrePlaneta = null;
             String TipoPlaneta = null;
             JPanel jPanelPosicion = (JPanel) jPanel1.getComponent(i);
             for (int j = 0; j < jPanelPosicion.getComponents().length; j++) {
-                
-               
-                
-                if (jPanelPosicion.getComponent(j) instanceof JTextField){
-                  
-                    NombrePlaneta=((JTextField)jPanelPosicion.getComponent(j)).getText(); 
-                     Nplanetas.add(NombrePlaneta);
-                    if(NombrePlaneta.length()==0){
-                     JOptionPane.showMessageDialog(null, "Debes poner un nombre a tu equipo", "Error", JOptionPane.ERROR_MESSAGE);   
- 
+
+                if (jPanelPosicion.getComponent(j) instanceof JTextField) {
+
+                    NombrePlaneta = ((JTextField) jPanelPosicion.getComponent(j)).getText();
+                    Nplanetas.add(NombrePlaneta);
+                    if (NombrePlaneta.length() == 0) {
+                        JOptionPane.showMessageDialog(null, "Debes poner un nombre a tu equipo", "Error", JOptionPane.ERROR_MESSAGE);
+
                         return;
-                        
+
                     }
-                } 
-               else if (jPanelPosicion.getComponent(j) instanceof JComboBox){
-                    TipoPlaneta=(String) ((JComboBox)jPanelPosicion.getComponent(j)).getSelectedItem();
-                   // System.out.println(Arrays.toString((String[])combo.getSelectedItem()));
-                    System.out.println("Planeta "+(contJugadores)+":"+NombrePlaneta);
-                   
-                    
-                } 
-               else if (jPanelPosicion.getComponent(j) instanceof JComboBox){
-            
-                   // System.out.println(Arrays.toString((String[])combo.getSelectedItem()));
-                   // System.out.println("Planeta "+(contJugadores)+":"+NombrePlaneta);
-                    System.out.println( ((JComboBox)jPanelPosicion.getComponent(j)).getSelectedItem());
+                } else if (jPanelPosicion.getComponent(j) instanceof JComboBox) {
+                    TipoPlaneta = (String) ((JComboBox) jPanelPosicion.getComponent(j)).getSelectedItem();
+                    // System.out.println(Arrays.toString((String[])combo.getSelectedItem()));
+                    System.out.println("Planeta " + (contJugadores) + ":" + NombrePlaneta);
 
-                    
+                } else if (jPanelPosicion.getComponent(j) instanceof JComboBox) {
+
+                    // System.out.println(Arrays.toString((String[])combo.getSelectedItem()));
+                    // System.out.println("Planeta "+(contJugadores)+":"+NombrePlaneta);
+                    System.out.println(((JComboBox) jPanelPosicion.getComponent(j)).getSelectedItem());
+
                 }
-                 
+
             }
-         
-            
-       
-                 for (int j = 0; j < Nplanetas.size(); j++) {
 
-                 for (int k = 0; k < Nplanetas.size(); k++) {
-                     if (j!=k && Nplanetas.get(j).equals(Nplanetas.get(k))){
-                       
-            
-                     JOptionPane.showMessageDialog(null, "Se repite algun nombre", "Error", JOptionPane.ERROR_MESSAGE);  
-                      }
-                 }
+            final String cmp = NombrePlaneta;
+            if (planetas.stream().map(e -> e.getNombre()).anyMatch(m -> m.equals(cmp))) {
+                JOptionPane.showMessageDialog(null, "Se repite algun nombre", "Error", JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+
+            try {
+                if (NombrePlaneta.length() != 0 && TipoPlaneta != null) {
+                    planetas.add(control.getPartida().crearPlanetas(i, TipoPlaneta, NombrePlaneta));
 
                 }
-                 
-            
-                 
-              
-            
-            
-            try{
-               if(NombrePlaneta.length() != 0 && TipoPlaneta != null){
-               planetas.add(control.getPartida().crearPlanetas(i, TipoPlaneta,NombrePlaneta ));
-               
-           }
-            }catch(Exception e){
-                JOptionPane.showMessageDialog(null, "Debes elegir tipo de planeta", "Error", JOptionPane.ERROR_MESSAGE);   
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "Debes elegir tipo de planeta", "Error", JOptionPane.ERROR_MESSAGE);
                 return;
-                   
-                   } 
-           
-            
-            
-           
-             
-               
-           
-            
-            
-         
-                
-           
-     
-            
-         this.setVisible(false);
-        control.empezar(planetas);
-       
-         this.setExtendedState(ICONIFIED);
-         
-        dispose();
-         
+
+            }
+
         }
-        
+        this.setVisible(false);
+        control.empezar(planetas);
+
+        this.setExtendedState(ICONIFIED);
+
+        dispose();
+
+
     }//GEN-LAST:event_btn_createActionPerformed
-
-
-    
 
     /**
      * @param args the command line arguments
@@ -335,24 +283,26 @@ public class CrearEquipos extends javax.swing.JFrame {
 }
 
 class JTextFieldLimit extends PlainDocument {
-  private int limit;
-  JTextFieldLimit(int limit) {
-    super();
-    this.limit = limit;
-  }
 
-  JTextFieldLimit(int limit, boolean upper) {
-    super();
-    this.limit = limit;
-  }
+    private int limit;
 
-  public void insertString(int offset, String str, AttributeSet attr) throws BadLocationException {
-    if (str == null)
-      return;
-
-    if ((getLength() + str.length()) <= limit) {
-      super.insertString(offset, str, attr);
+    JTextFieldLimit(int limit) {
+        super();
+        this.limit = limit;
     }
-  }
-}
 
+    JTextFieldLimit(int limit, boolean upper) {
+        super();
+        this.limit = limit;
+    }
+
+    public void insertString(int offset, String str, AttributeSet attr) throws BadLocationException {
+        if (str == null) {
+            return;
+        }
+
+        if ((getLength() + str.length()) <= limit) {
+            super.insertString(offset, str, attr);
+        }
+    }
+}
